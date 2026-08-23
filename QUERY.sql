fcacd1b123c7303709384a236b71442337c4c1ab
Md Jasim Uddin <mjunachole97@gmail.com>
2026-08-24 00:06:22 +0600

    user table create

--- QUERY.sql
+++ QUERY.sql
@@ -4,4 +4,14 @@
 create database football_ticket
 
 
+-- =========================================================================
+-- 1. CREATE USERS TABLE
+-- =========================================================================
 
+create table users (
+    user_id serial primary key,
+    full_name varchar(25) not null,
+    email varchar(25) unique not null ,
+    role varchar(20) check( role in('Football Fan', 'Ticket Manager')) default 'Football Fan' not null,
+    phone_number varchar(15)
+);
