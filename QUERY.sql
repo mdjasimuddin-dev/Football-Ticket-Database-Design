490c5faa291725300537edfccb83b459c2b5de65
Md Jasim Uddin <mjunachole97@gmail.com>
2026-08-24 00:26:07 +0600

    bookings  table create

--- QUERY.sql
+++ QUERY.sql
@@ -28,3 +28,17 @@ create table users (
     base_ticket_price int not null,
     match_status varchar(15) check(match_status in('Available', 'Selling Fast', 'Sold Out')) default  'Available' not null
 );
+
+-- =========================================================================
+-- 3. CREATE BOOKINGS TABLE
+-- =========================================================================
+
+create table bookings (
+    booking_id serial primary key,
+    user_id int references users(user_id) on delete cascade,
+    match_id int references matches(match_id) on delete cascade,
+    seat_number varchar(10),
+    payment_status varchar(20) check (payment_status in ('Pending', 'Confirmed', 'Cancelled', 'Refunded')),
+    total_cost int not null
+    
+);
